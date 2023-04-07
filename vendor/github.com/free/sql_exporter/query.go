4e222d00a3225bb9a45045120133d3104cf5250d
igor-antonio <igor.antonio@outlook.com>
2023-04-07 08:42:52 -0300

    Trying to increase the query timeout.

--- vendor/github.com/free/sql_exporter/query.go
+++ vendor/github.com/free/sql_exporter/query.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"time"
 
 	"github.com/free/sql_exporter/config"
 	"github.com/free/sql_exporter/errors"
@@ -74,8 +73,6 @@ func setColumnType(logContext, columnName string, ctype columnType, columnTypes 
 
 // Collect is the equivalent of prometheus.Collector.Collect() but takes a context to run in and a database to run on.
 func (q *Query) Collect(ctx context.Context, conn *sql.DB, ch chan<- Metric) {
-	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
-	defer cancel()
 	if ctx.Err() != nil {
 		ch <- NewInvalidMetric(errors.Wrap(q.logContext, ctx.Err()))
 		return
@@ -111,8 +108,6 @@ func (q *Query) Collect(ctx context.Context, conn *sql.DB, ch chan<- Metric) {
 
 // run executes the query on the provided database, in the provided context.
 func (q *Query) run(ctx context.Context, conn *sql.DB) (*sql.Rows, errors.WithContext) {
-	ctx, cancel := context.WithTimeout(context.Background(), 600*time.Second)
-	defer cancel()
 	if q.conn != nil && q.conn != conn {
 		panic(fmt.Sprintf("[%s] Expecting to always run on the same database handle", q.logContext))
 	}
